0b9a1981acefe7f34c149fc8fdc2fb7d5ed3b811
arunchavan89 <arunchavan89@gmail.com>
2021-01-06 18:32:25 +0100

    segmentation of the pointcloud plane and obstacle

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,14 +42,19 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
-    // TODO:: Create lidar sensor 
+    // DONE:: Create lidar sensor
+    Lidar *lidar = new Lidar(cars, 0);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
+    renderPointCloud(viewer, inputCloud, "PointCloudName");
 
     // TODO:: Create point processor
-    // Manjeet will do it later
-  
+    ProcessPointClouds<pcl::PointXYZ> pointProcessor;
+    std::pair<typename pcl::PointCloud<pcl::PointXYZ>::Ptr, typename pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
 }
 
 
